015ac3e9503534935cf6fa83a1b377284c6d7496
Mohit Saud <mohit@prabidhilabs.com>
2024-12-08 19:34:26 +0545

    Add Call Button

--- components/CallButton.tsx
+++ components/CallButton.tsx
@@ -4,9 +4,9 @@ const number = '+977015920126';
 
 const CallButton = () => {
   return (
-    <a href={`tel:${number}`} className="fixed bottom-5 left-2">
-      <div className="flex items-center justify-center w-16 h-16 bg-green-500 rounded-full shadow-lg transition-transform transform hover:scale-110 animate-pulse">
-        <FaCommentDots className="text-white text-3xl" />
+    <a href={`tel:${number}`} className="fixed bottom-5 right-0">
+      <div className="flex items-center justify-center w-12 h-12 bg-green-500 rounded-full shadow-lg transition-transform transform hover:scale-110 animate-pulse">
+        <FaCommentDots className="text-white text-2xl" />
       </div>
     </a>
   );
