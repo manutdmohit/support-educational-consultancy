3c6a9011194c869e0704033d0ab1550ee05df2ff
Mohit Saud <mohit@prabidhilabs.com>
2024-12-08 19:58:37 +0545

    Fix click error on mobile devices

--- components/CallButton.tsx
+++ components/CallButton.tsx
@@ -5,8 +5,8 @@ const number = '+977015920126';
 const CallButton = () => {
   return (
     <a href={`tel:${number}`} className="fixed bottom-5 right-0">
-      <div className="flex items-center justify-center w-12 h-12 bg-green-500 rounded-full shadow-lg transition-transform transform hover:scale-110 animate-pulse">
-        <FaCommentDots className="text-white text-2xl" />
+      <div className="flex items-center justify-center w-16 h-16 md:w-12 md:h-12 bg-green-500 rounded-full shadow-lg transition-transform transform hover:scale-110 animate-pulse">
+        <FaCommentDots className="text-white text-3xl md:text-2xl" />
       </div>
     </a>
   );
