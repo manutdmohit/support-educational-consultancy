11e00d14b966f7b597976ca773f6bc96e0b0336f
Mohit Saud <saudmohit@gmail.com>
2024-12-28 17:50:51 +0545

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className="absolute inset-0 z-0 overflow-hidden"
+        className="absolute inset-0 overflow-hidden"
       >
         <video
           autoPlay
